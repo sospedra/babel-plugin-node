417990db8414a3a5b9d5c9255f8a4378c48a9138
Rubén Sospedra <r.sospedra@mytaxi.com>
2018-10-05 14:25:36 +0200

    feat: Add global.location polyfill for serviceworker

--- polyfill.js
+++ polyfill.js
@@ -1,3 +1,7 @@
 /* eslint-disable */
 import buffer from 'buffer'
 import process from 'process'
+
+global.location = global.location || {}
+global.location.protocol = global.location.protocol || 'http'
+
